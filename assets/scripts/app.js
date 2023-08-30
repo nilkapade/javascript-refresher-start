c3d5950ba4ebb181d6e9c240685c1d76930aecec
nilkapade <nilkapade@gmail.com>
2023-08-30 22:31:36 +0530

    Arrays and Destructuring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -31,3 +31,62 @@ import { apikey } from "./util.js";
 // u1.greet();
 
 //*** Arrays */
+
+// const hobbies = ["Sports", "Reading", "Cooking"];
+// console.log(hobbies[0])
+
+// hobbies.push("Trekking");
+// console.log(hobbies);
+
+// // const index = hobbies.findIndex((item) => {
+// //     return item === "Trekking";
+// // });
+
+// const index = hobbies.findIndex((item) => item === "Trekking");
+// console.log(index);
+
+// // const editedMap = hobbies.map((item) => item + "!");
+// // console.log(editedMap);
+
+// const editedMap = hobbies.map((item) => ({text: item}));
+// console.log(editedMap);
+
+// Destructuring
+
+// const [firstName, lastName] = ["Max", "Schwarzmuller"];
+// //const firstName = names[0];
+// //const lastName = name[1];
+// console.log(firstName);
+// console.log(lastName);
+
+// const {name, age} = {
+//     name: "XYZ",
+//     age: "123"
+// }
+
+// const {name: uName, age: UAge} = {
+//     name: "XYZ",
+//     age: "123"
+// }
+// //const name = user.name;
+// //const age = user.age;
+// console.log(uName);
+// console.log(UAge);
+
+// function storeOrder(order) {
+//     localStorage.setItem('id', order.id);
+//     localStorage.setItem('currency', order.currency);
+// }
+
+// function storeOrder({id, currency}) { // destructuring
+//     localStorage.setItem('id', id);
+//     localStorage.setItem('currency', currency);
+//     console.log(id);
+//     console.log(currency);
+//     console.log(amount);
+// }
+
+// storeOrder({id: 5, currency: 'USD', amount: 15.99}); 
+
+
+
