f384d5da8d14f0309118f973d2e9a87115b8de43
nilkapade <nilkapade@gmail.com>
2023-09-02 22:01:03 +0530

    formatting

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -194,7 +194,8 @@ import { apikey } from "./util.js";
 // Expected output: Array [2, 8, 18, 32]
 
 //Find Array.prototype.find()
-//The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+//The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, 
+//undefined is returned.
 // If you need the index of the found element in the array, use findIndex().
 // If you need to find the index of a value, use indexOf(). (It's similar to findIndex(), but checks each element for equality with the value instead of using a testing function.)
 // If you need to find if a value exists in an array, use includes(). Again, it checks each element for equality with the value instead of using a testing function.
@@ -208,7 +209,8 @@ import { apikey } from "./util.js";
 // Expected output: 12
 
 //Array.prototype.findIndex()
-// The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.
+// The findIndex() method of Array instances returns the index of the first element in an array that satisfies the provided testing function. 
+// If no elements satisfy the testing function, -1 is returned.
 // See also the find() method, which returns the first element that satisfies the testing function (rather than its index).
 
 // const array1 = [5, 12, 8, 130, 44];
